de1b394bf198ca25002bd84f9b5c254e0fe21ca6
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-11-05 13:03:13 +0200

    [add] - getCountries and getStates methods added to luv2shop-form.service.ts

--- src/app/services/luv2shop-form.service.ts
+++ src/app/services/luv2shop-form.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs";
+import {map, Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {State} from "../common/state";
+
+// unwraps the json from Spring Data REST _embedded entry
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2shopFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/countries';
+  private stateUrl = 'http://localhost:8080/states';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    const searchStateUrl = `${this.stateUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStateUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
@@ -18,7 +53,7 @@ export class Luv2shopFormService {
       data.push(theMonth);
     }
 
-    return of(data); // of operator wraps an object as an Observable
+    return of(data); // "of" operator wraps an object as an Observable
   }
 
   getCreditCardYears(): Observable<number[]> {
