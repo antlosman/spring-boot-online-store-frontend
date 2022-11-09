8a501d103ad7cc8bd44ecd5e7a2851436d72092f
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-11-09 14:21:20 +0200

    [add] - okta dependencies added

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa76w9t56sMXJQli5d7',
+    issuer: 'https://dev-23349764.okta.com/oauth/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
