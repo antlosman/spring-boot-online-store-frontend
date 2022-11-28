e6687586b185e44a8d4c8d6bb251d7a6882cb745
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-11-28 11:09:18 +0200

    [add] - OrderHistory component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { OktaAuth } from "@okta/okta-auth-js";
 import myAppConfig from "./config/my-app-config";
 import { ProductService } from "./services/product.service";
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -65,7 +66,8 @@ const routes: Routes = [
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
