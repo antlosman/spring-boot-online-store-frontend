155c019d19a9b30c850762a0fbcd90bcb30b46d1
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-12-15 16:51:34 +0200

    [add] - secure communication with HTTPS

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa76w9t56sMXJQli5d7',
     issuer: 'https://dev-23349764.okta.com',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
